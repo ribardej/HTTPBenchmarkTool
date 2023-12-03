9871db3cc8dc2081b9ea678a69418fd2f8ecc2c2
ribardej <ribardej@student.cvut.cz>
2023-12-03 14:52:13 +0100

    new dummy version

--- examples/examples.jl
+++ examples/examples.jl
@@ -1,8 +1,6 @@
 using Revise
-using HTTPBenchmarkTool, HTTP
+using HTTPBenchmarkTool
 
-greet()
 
-@elapsed benchmark_dummy("http://localhost:3000", 100, 10000)
-
+benchmark_dummy_v3("http://localhost:3000", 100, 10000)
 
--- src/HTTPBenchmarkTool.jl
+++ src/HTTPBenchmarkTool.jl
@@ -1,13 +1,41 @@
 module HTTPBenchmarkTool
 
-using HTTP
+using HTTP, StatsBase, Plots
 
-export greet, benchmark_dummy
+export benchmark_dummy_v3
 
-greet() = println("HTTPStart")
+struct MyResponseData
+    time::Float64
+    status::Int16
+end
 
-function benchmark_dummy(url::String, client_n::Int64, req_n::Int64)
-    return @time asyncmap(x->HTTP.request("GET", url), 1:req_n; ntasks=client_n)
+
+function benchmark_dummy_v3(url::String, client_n::Int64, req_n::Int64; kwargs...)
+    data = asyncmap(x -> make_request(url), 1:req_n; ntasks=client_n)
+    time_data = [x.time for x in data]
+    requests_per_second = req_n / sum(time_data)
+    mean_response_time = sum(time_data) / req_n
+    hist = fit(Histogram, time_data, nbins=200)
+    visualize_results(requests_per_second, mean_response_time, hist; kwargs...)
+    return Dict(
+        "RequestsPerSecond" => requests_per_second,
+        "MeanResponseTime" => mean_response_time,
+        "ResponseTimeHistogram" => hist
+    )
+end
+
+function make_request(url::String)
+    ela = @elapsed res = HTTP.request("GET", url)
+    return MyResponseData(ela, res.status)
+end 
+
+function visualize_results(requests_per_second, mean_response_time, hist; kwargs...)
+    plot(hist, xlabel="Response Time", ylabel="Frequency", title="Response Time Distribution")
+    if haskey(kwargs, :saveplot) && kwargs[:saveplot]
+        savefig("response_time_distribution.png")
+    else
+        display(Plots.current())
+    end
 end
 
 
