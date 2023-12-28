b9181b22d852b89de14511762e39be34b0f9d893
ribardej <ribardej@student.cvut.cz>
2023-12-29 00:16:40 +0100

    v1.0

--- examples/examples.jl
+++ examples/examples.jl
@@ -1,6 +1,11 @@
 using Revise
-using HTTPBenchmarkTool
+using HTTPBenchmarkTool, StatsPlots, DataFrames
 
 
-benchmark_dummy_v3("http://localhost:3000", 100, 100000)
+# @time val = benchmark_dummy_v4("http://google.com", 25, 1000)
+# @time val = benchmark_dummy_v3("http://google.com", 25, 1000)
+
+bun_data = benchmark("http://localhost:3000"; req_each=100000, client_ns=10:10:100)
+deno_data = benchmark("http://localhost:3001"; req_each=10000, client_ns=10:10:100)
+node_data = benchmark("http://localhost:3002"; req_each=10000, client_ns=10:10:100)
 
--- src/HTTPBenchmarkTool.jl
+++ src/HTTPBenchmarkTool.jl
@@ -1,43 +1,60 @@
 module HTTPBenchmarkTool
 
-using HTTP, StatsBase, Plots
+using HTTP, StatsBase, Plots, StatsPlots, DataFrames
 
-export benchmark_dummy_v3
+export benchmark, benchmark_specialized
 
 struct MyResponseData
-    time::Float64
+    time::Float16
     status::Int16
 end
 
+function benchmark(url::String; req_each=10000, client_ns=10:10:100, kwargs...)
+    df = DataFrame(NumberOfClients = Int16[], ElapsedTime = Float64[], Status = Int16[])
+    for client_n in client_ns
+        println("Benchmarking with $client_n clients")
+        val = helper_divide_conquer(url, client_n, req_each; kwargs...)
+        df = vcat(df, DataFrame(val))
+    end
+    # @df df marginalhist(:NumberOfClients, :ElapsedTime)
+    return df
+end
 
-function benchmark_dummy_v3(url::String, client_n::Int64, req_n::Int64; kwargs...)
+function helper_pure_async(url::String, client_n::Int64, req_n::Int64; kwargs...)
     data = asyncmap(x -> make_request(url), 1:req_n; ntasks=client_n)
-    time_data = [x.time for x in data]
-    requests_per_second = req_n / sum(time_data)
-    mean_response_time = sum(time_data) / req_n
-    hist = fit(Histogram, time_data, nbins=20)
-    visualize_results(requests_per_second, mean_response_time, hist; kwargs...)
-    println("Time elapsed: ", sum(time_data))
     return Dict(
-        "RequestsPerSecond" => requests_per_second,
-        "MeanResponseTime" => mean_response_time,
-        "ResponseTimeHistogram" => hist
+        "NumberOfClients" => [Int16(client_n) for _ in 1:req_n],
+        "ElapsedTime" => [x.time for x in data],
+        "Status" => [z.status for z in data]
+    )
+end
+
+function helper_divide_conquer(url::String, client_n::Int64, req_n::Int64; kwargs...)
+    reqs_per_client = Int64(floor(req_n/client_n))
+    inputs = ones(Int64, client_n) .* reqs_per_client
+    inputs[1] = req_n - ((client_n - 1)*reqs_per_client)
+    data = asyncmap(x -> make_n_requests(url, x), inputs; ntasks=client_n)
+    flat_data = reduce(vcat, data)
+    return Dict(
+        "NumberOfClients" => [Int16(client_n) for _ in 1:req_n],
+        "ElapsedTime" => [x.time for x in flat_data],
+        "Status" => [z.status for z in flat_data]
     )
 end
 
 function make_request(url::String)
     ela = @elapsed res = HTTP.request("GET", url)
-    return MyResponseData(ela, res.status)
+    return MyResponseData(Float16(round(ela, sigdigits=2)), res.status)
 end 
 
-function visualize_results(requests_per_second, mean_response_time, hist; kwargs...)
-    plot(hist, xlabel="Response Time", ylabel="Frequency", title="Response Time Distribution")
-    if haskey(kwargs, :saveplot) && kwargs[:saveplot]
-        savefig("response_time_distribution.png")
-    else
-        display(Plots.current())
-    end
-end
 
+function make_n_requests(url::String, n::Int64)
+    ret = Array{MyResponseData}(undef, n)
+    for i in 1:n
+        ela = @elapsed res = HTTP.request("GET", url)
+        ret[i] = MyResponseData(ela, res.status)
+    end
+    return ret
+end 
 
 end
